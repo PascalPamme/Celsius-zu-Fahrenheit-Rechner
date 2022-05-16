e6cf7370bffd850896722f458fab31dfbc329e88
PascalPamme <PascalPamme@gmx.de>
2022-05-16 16:53:33 +0200

    added lint

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-var readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync');
 
-console.log("Type in Celsius and press enter to calculate Fahrenheit. Press e to exit")
+console.log('Type in Celsius and press enter to calculate Fahrenheit. Press e to exit');
 
-function ftoC(c){
-    var f = c * (9 / 5) + 32;    
-    return f
-
-};
-function convertStringToNumber(aNumber){
-    return Number(aNumber)
+function ftoC(c) {
+    const f = c * (9 / 5) + 32;
+    return f;
+}
+function convertStringToNumber(aNumber) {
+    return Number(aNumber);
 }
 
-function calc(){
+function calc() {
+    let celsius = readlineSync.question('enter Celsius:');
 
-var celsius = readlineSync.question("enter Celsius:");
-
-if (celsius === "e"){
-    console.log("Thanks for using my app");
-    process.exit(1);    
-}
-
-celsius = convertStringToNumber(celsius)
-var fahrenheit = ftoC(celsius)
-if (isNaN(celsius)){
-    console.log("input must be a number");
-}
-else
-     console.log("in Amerika it is " + fahrenheit + "F");
-    if (celsius === "e"){
+    if (celsius === 'e') {
+        console.log('Thanks for using my app');
         process.exit(1);
     }
 
-    else {
-    calc();
+    celsius = convertStringToNumber(celsius);
+    const fahrenheit = ftoC(celsius);
+    if (isNaN(celsius)) {
+        console.log('input must be a number');
+    } else {
+        console.log('in Amerika it is ' + fahrenheit + 'F');
+    }
+    if (celsius === 'e') {
+        process.exit(1);
+    } else {
+        calc();
+    }
 }
-};
 calc();
-
-
-
